f47e944a9152cbc7c910ca5f1be55c48920e7341
xuhuihui <494655043@qq.com>
2020-07-21 11:24:50 +0800

    function spec: should change captured variable

--- __tests__/pieceOfCake/function_spec.js
+++ __tests__/pieceOfCake/function_spec.js
@@ -145,7 +145,7 @@ describe('for function', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = 'Changed';
     // --end->
 
     expect(guessIfIAmChanged).toEqual(expected);
