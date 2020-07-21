48846b242a8045b88ff77ca0cf68223884bce0d9
xuhuihui <494655043@qq.com>
2020-07-21 11:05:52 +0800

    event spec: should invoke multiple times

--- __tests__/pieceOfCake/event_spec.js
+++ __tests__/pieceOfCake/event_spec.js
@@ -34,7 +34,10 @@ describe('for event', () => {
       .then(() => {
         // <--start
         // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
+        const expected = [
+          'I have been clicked',
+          'I have been clicked',
+        ];
         // --end->
 
         expect(logs).toEqual(expected);
