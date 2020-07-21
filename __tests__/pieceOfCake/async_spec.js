35596d9b3a77c1056cad7f5145a7fb8b07c21b92
xuhuihui <494655043@qq.com>
2020-07-21 10:52:37 +0800

    async spec: should propagate the error as the way of the sync code

--- __tests__/pieceOfCake/async_spec.js
+++ __tests__/pieceOfCake/async_spec.js
@@ -99,7 +99,12 @@ describe('for asynchronous', () => {
       .then(() => {
         // <--start
         // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
+        const expected = [
+          'Caught! >_<',
+          'Continued',
+          'Another continued',
+          'Error handled: Holy ~',
+        ];
         // --end->
         expect(logs).toEqual(expected);
         done();
